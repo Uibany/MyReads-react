785f1acd4408d7d5ff0db745d234f5b0d2442942
Huaqin <53359929+Uibany@users.noreply.github.com>
2021-02-27 18:13:12 +0800

    Updates

---
+++ precache-manifest.158d75bf0ce24a0eb549a165b7359b9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f08d8f090ccf91c4959",
+    "url": "/myreads-app/static/css/main.f83cea69.chunk.css"
+  },
+  {
+    "revision": "9f08d8f090ccf91c4959",
+    "url": "/myreads-app/static/js/main.9f08d8f0.chunk.js"
+  },
+  {
+    "revision": "c24d30b14efb9ea956f9",
+    "url": "/myreads-app/static/css/1.b8b0e07d.chunk.css"
+  },
+  {
+    "revision": "c24d30b14efb9ea956f9",
+    "url": "/myreads-app/static/js/1.c24d30b1.chunk.js"
+  },
+  {
+    "revision": "1500c43aa4a67be84717",
+    "url": "/myreads-app/static/js/runtime~main.1500c43a.js"
+  },
+  {
+    "revision": "f89ff9d702e455e4a07b938a4179355b",
+    "url": "/myreads-app/static/media/background.f89ff9d7.jpeg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "ac50729024d98c309685b4b463b62efa",
+    "url": "/myreads-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyReads-react/precache-manifest.8503843f3287a840b0d26f689008cbd9.js"
+  "/myreads-app/precache-manifest.158d75bf0ce24a0eb549a165b7359b9a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MyReads-react/index.html", {
+workbox.routing.registerNavigationRoute("/myreads-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
