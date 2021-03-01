d626b0af02b7e8cb92d5c209a378934b3f3215e4
Huaqin <53359929+Uibany@users.noreply.github.com>
2021-03-01 18:15:52 +0800

    chore: deploy the latest version

--- build/precache-manifest.4b6d547cd6d2c12f5c8f61eddf779150.js
+++ build/precache-manifest.4b6d547cd6d2c12f5c8f61eddf779150.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "d7ea12ad65d1d13181b8",
-    "url": "/myreads-app/static/css/main.e348da65.chunk.css"
+    "revision": "e9b638d1442de1f295d7",
+    "url": "/myreads-app/static/css/main.1955a5f8.chunk.css"
   },
   {
-    "revision": "d7ea12ad65d1d13181b8",
-    "url": "/myreads-app/static/js/main.d7ea12ad.chunk.js"
+    "revision": "e9b638d1442de1f295d7",
+    "url": "/myreads-app/static/js/main.e9b638d1.chunk.js"
   },
   {
-    "revision": "b032f2eb9344bb808f82",
+    "revision": "e7a8de2ca8a38eb336f6",
     "url": "/myreads-app/static/css/1.b8b0e07d.chunk.css"
   },
   {
-    "revision": "b032f2eb9344bb808f82",
-    "url": "/myreads-app/static/js/1.b032f2eb.chunk.js"
+    "revision": "e7a8de2ca8a38eb336f6",
+    "url": "/myreads-app/static/js/1.e7a8de2c.chunk.js"
   },
   {
     "revision": "1500c43aa4a67be84717",
@@ -24,15 +24,15 @@ self.__precacheManifest = [
     "url": "/myreads-app/static/media/background.f89ff9d7.jpeg"
   },
   {
-    "revision": "bb9363b86b02777d23523851826414e0",
-    "url": "/myreads-app/static/media/add.bb9363b8.svg"
-  },
-  {
     "revision": "3687aa988bec28f17ae39153db1646c3",
     "url": "/myreads-app/static/media/arrow-back.3687aa98.svg"
   },
   {
-    "revision": "83c8732d0012077b03fcf628bc3985bc",
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "41bd165288830ee1b8d9b5025d609f8d",
     "url": "/myreads-app/index.html"
   }
 ];
